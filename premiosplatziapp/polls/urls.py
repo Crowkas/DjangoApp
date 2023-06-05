f596dfb1ecb94ff71e1f4c12458c156bc267da48
Crowkas <nicolas019724@gmail.com>
2023-06-05 16:39:53 -0400

    Save III, generic views, function based views, templates, admin control

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name = 'index')
+    #ex: /polls/
+    path('', views.IndexView.as_view(), name = 'index'),
+    #ex: /polls/detail/question_id/
+    path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
+    #ex: /polls/results/question_id/
+    path("results/<int:pk>/", views.ResultView.as_view, name="results"),
+    #ex: /polls/vote/question_id/
+    path("vote/<int:question_id>/", views.vote, name="vote"),
 ]
