aa5722fdceba73ef233a9920d524d782255cb20d
Crowkas <nicolas019724@gmail.com>
2023-06-06 17:03:11 -0400

    Save IV NEW TESTING MDOE, ADMIN MODE, CSS, NEW STYLES

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     #ex: /polls/detail/question_id/
     path("detail/<int:pk>/", views.DetailView.as_view(), name="detail"),
     #ex: /polls/results/question_id/
-    path("results/<int:pk>/", views.ResultView.as_view, name="results"),
+    path("results/<int:pk>/", views.ResultView.as_view(), name="results"),
     #ex: /polls/vote/question_id/
     path("vote/<int:question_id>/", views.vote, name="vote"),
 ]
